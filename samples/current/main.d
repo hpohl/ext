472c9014ca652553e05945094041146e09b552cb
Henning Pohl <henning@still-hidden.de>
2012-09-27 22:27:09 +0200

    more project files

---
+++ samples/current/main.d
@@ -0,0 +1,40 @@
+module samples.current.main;
+
+import std.stdio;
+
+import ext.gui.layout;
+import ext.gui.picture;
+import ext.resource.image;
+import ext.resource.path;
+import ext.resource.pool;
+import ext.resource.resource;
+import ext.window.freeglut.window;
+
+
+void createResources() {
+	auto pool = new Pool("packages");
+	auto img = new Image(Path("fun:tux"));
+	img.loadFromFile("images/tux.png");
+	pool.save(img);
+	pool.write();
+}
+
+void main() {
+	// createResources();
+	
+	auto pool = new Pool("packages");
+	auto img = pool.load!Image(Path("fun:tux"));
+	
+	
+	////////////////////////////////////////
+	auto win = new Window;
+	
+	auto layout = new Layout(win.inputDevice);
+	auto pic = new Picture(img.getTexture(win.target.context));
+	
+	while (true) {
+		layout.draw(win.target);
+		
+		win.update();
+	}
+}
